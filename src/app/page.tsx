e665de171f2a34a2f13ed6d2b330d9489494817b
IgnacioOE <A01741599@tec.mx>
2025-03-03 07:36:52 -0600

    clima

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { useState } from "react";
+
+export default function Home() {
+  const [ciudad, setCiudad] = useState({
+      name: '',
+      temp: '',
+    });
+  
+
+  return (
+    ""
+  );
+}
